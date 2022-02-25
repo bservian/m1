e6d8b16077d1b4652c25ea76e5db47d82f9ff88f
Benjamin Servian <ben.servian@gmail.com>
2022-02-25 16:34:24 +0100

    ajout des activity

---
+++ app/src/main/java/com/example/montauru/AjouterMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.montauru;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AjouterMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_menu);
+    }
+}
---
+++ app/src/main/java/com/example/montauru/MenusCrousActivity.java
@@ -0,0 +1,13 @@
+package com.example.montauru;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenusCrousActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menus_crous);
+    }
+}
